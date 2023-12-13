c899120daf24ae5d54dd76c656d134d14e8c1080
hakyung <diveindeep.dev@gmail.com>
2023-12-13 17:58:18 +0900

    Add statics and test item

---
+++ src/js/burger.js
@@ -0,0 +1,14 @@
+const BURGER = [
+  { radius: 30, point: 1, img: '/assets/burger/burger1.png' },
+  { radius: 40, point: 2, img: '/assets/burger/burger2.png' },
+  { radius: 50, point: 3, img: '/assets/burger/burger3.png' },
+  { radius: 60, point: 4, img: '/assets/burger/burger4.png' },
+  { radius: 70, point: 5, img: '/assets/burger/burger5.png' },
+  { radius: 70, point: 6, img: '/assets/burger/burger6.png' },
+  { radius: 80, point: 7, img: '/assets/burger/burger7.png' },
+  { radius: 90, point: 8, img: '/assets/burger/burger8.png' },
+  { radius: 100, point: 9, img: '/assets/burger/burger9.png' },
+  { radius: 110, point: 10, img: '/assets/burger/burger10.png' },
+];
+
+export default BURGER;
--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
+import BURGER from './burger.js';
 import { COLORS, SIZES } from '../styles/variable.js';
-const { Engine, Render, Runner } = Matter;
+const { Engine, Render, Runner, Composite, Bodies } = Matter;
 
 export const GAME = {
   width: 600,
@@ -20,6 +21,73 @@ const render = Render.create({
 
 Render.run(render);
 Runner.run(engine);
+
+// 직사각형으로 벽 만들기
+// Matter.Bodies.rectangle(x, y, width, height, [options])
+const wallThickness = SIZES.wallThickness;
+const scoreHeight = SIZES.scoreHeight;
+const wallOptions = {
+  isStatic: true,
+  render: { fillStyle: COLORS.gameStatic },
+};
+
+const wallStatics = [
+  // ScoreLine
+  Bodies.rectangle(GAME.width / 2, scoreHeight, GAME.width, 1, {
+    isStatic: true,
+    isSensor: true,
+  }),
+  //Left
+  Bodies.rectangle(
+    wallThickness / 2,
+    GAME.height / 2,
+    wallThickness,
+    GAME.height,
+    wallOptions,
+  ),
+  // Right
+  Bodies.rectangle(
+    GAME.width - wallThickness / 2,
+    GAME.height / 2,
+    wallThickness,
+    GAME.height,
+    wallOptions,
+  ),
+  // Bottom
+  Bodies.rectangle(
+    GAME.width / 2,
+    GAME.height - wallThickness,
+    GAME.width,
+    wallThickness * 2,
+    wallOptions,
+  ),
+];
+
+Composite.add(engine.world, wallStatics);
+
+// 게임 요소 생성
+const newIngredient = () => {
+  const index = Math.floor(Math.random() * 4);
+  const ingredient = BURGER[index];
+  const scale = (ingredient.radius * 2) / 1000;
+
+  //Matter.Bodies.circle(x, y, radius, [options], [maxSides])
+  const body = Bodies.circle(
+    GAME.width / 2,
+    scoreHeight - ingredient.radius,
+    ingredient.radius,
+    {
+      render: {
+        sprite: { texture: ingredient.img, xScale: scale, yScale: scale },
+      },
+      restitution: 0.3,
+    },
+  );
+
+  Composite.add(engine.world, body);
+};
+
+newIngredient();
 
 const resize = () => {
   const screenWidth = window.innerWidth;

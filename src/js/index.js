1226c8b510f8fa93401b7bd26692a2fb12cb16a0
hakyung <diveindeep.dev@gmail.com>
2023-12-13 22:47:10 +0900

    Implement dropItem with mouse input in matter.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,17 @@
 import BURGER from './burger.js';
 import { COLORS, SIZES } from '../styles/variable.js';
-const { Engine, Render, Runner, Composite, Bodies } = Matter;
+const { Engine,  Render,  Runner,  Composite,  Bodies,  Mouse,  MouseConstraint,  Events } = Matter;
+
+const STATUS = {
+  READY: 1,
+  DROP: 2,
+};
 
 export const GAME = {
   width: 600,
   height: 900,
+  status: STATUS.READY,
+  score: 0,
 };
 
 const engine = Engine.create();
@@ -65,18 +72,31 @@ const wallStatics = [
 
 Composite.add(engine.world, wallStatics);
 
-// 게임 요소 생성
-const newIngredient = () => {
-  const index = Math.floor(Math.random() * 4);
+let readyItem = null;
+let currentIndex = null;
+
+// 아이템 생성
+const newIngredient = (x, isReady = true, latestIndex) => {
+  const index =
+    GAME.score === 0
+      ? 0
+      : latestIndex !== undefined
+      ? latestIndex
+      : Math.floor(Math.random() * 4);
+
+  currentIndex = index;
+
   const ingredient = BURGER[index];
   const scale = (ingredient.radius * 2) / 1000;
 
   //Matter.Bodies.circle(x, y, radius, [options], [maxSides])
   const body = Bodies.circle(
-    GAME.width / 2,
+    x,
     scoreHeight - ingredient.radius,
     ingredient.radius,
     {
+      isSleeping: isReady,
+      isStatic: isReady,
       render: {
         sprite: { texture: ingredient.img, xScale: scale, yScale: scale },
       },
@@ -84,10 +104,59 @@ const newIngredient = () => {
     },
   );
 
-  Composite.add(engine.world, body);
+  return body;
 };
 
-newIngredient();
+// 아이템 drop
+const dropItem = (x) => {
+  if (GAME.status !== STATUS.READY) return;
+
+  GAME.status = STATUS.DROP;
+
+  // readyItem과 같은 모양의 droppedItem을 만들어 떨어뜨린 후 기존 readyItem 삭제
+  const droppedItem = newIngredient(x, false, currentIndex);
+  Composite.add(engine.world, droppedItem);
+  GAME.score += BURGER[currentIndex].score;
+  Composite.remove(engine.world, readyItem);
+
+  // 새로운 readyItem 할당 후 0.5초 후 표시
+  readyItem = newIngredient(x);
+  setTimeout(() => {
+    if (GAME.status === STATUS.DROP) {
+      Composite.add(engine.world, readyItem);
+      GAME.status = STATUS.READY;
+    }
+  }, 500);
+};
+
+const startGame = () => {
+  if (GAME.score > 0) return;
+  readyItem = newIngredient(GAME.width / 2);
+  Composite.add(engine.world, readyItem);
+};
+
+startGame();
+
+// 마우스 설정
+const mouse = Mouse.create(render.canvas);
+const mouseConstraint = MouseConstraint.create(engine, {
+  mouse: mouse,
+  constraint: {
+    stiffness: 0.1,
+  },
+});
+
+render.mouse = mouse;
+
+// 마우스 이동 or 터치 이동
+Events.on(mouseConstraint, 'mousemove', (e) => {
+  readyItem.position.x = e.mouse.position.x;
+});
+
+// 마우스 클릭 or 터치 후 drop
+Events.on(mouseConstraint, 'mouseup', (e) => {
+  dropItem(e.mouse.position.x);
+});
 
 const resize = () => {
   const screenWidth = window.innerWidth;

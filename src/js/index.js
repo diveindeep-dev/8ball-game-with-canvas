21b258b1fa8b8f6820fac81052caecd2d0fda49f
hakyung <diveindeep.dev@gmail.com>
2023-12-14 13:59:49 +0900

    Add collision event

--- src/js/index.js
+++ src/js/index.js
@@ -76,33 +76,29 @@ let readyItem = null;
 let currentIndex = null;
 
 // 아이템 생성
-const newIngredient = (x, isReady = true, latestIndex) => {
+const newIngredient = (position, fixedIndex, option) => {
   const index =
     GAME.score === 0
       ? 0
-      : latestIndex !== undefined
-      ? latestIndex
+      : fixedIndex !== undefined
+      ? fixedIndex
       : Math.floor(Math.random() * 4);
-
-  currentIndex = index;
 
   const ingredient = BURGER[index];
   const scale = (ingredient.radius * 2) / 1000;
+  const positionY =
+    position.y !== undefined ? position.y : scoreHeight - ingredient.radius;
+  const options = option || { isSleeping: true, isStatic: true };
 
   //Matter.Bodies.circle(x, y, radius, [options], [maxSides])
-  const body = Bodies.circle(
-    x,
-    scoreHeight - ingredient.radius,
-    ingredient.radius,
-    {
-      isSleeping: isReady,
-      isStatic: isReady,
-      render: {
-        sprite: { texture: ingredient.img, xScale: scale, yScale: scale },
-      },
-      restitution: 0.3,
+  const body = Bodies.circle(position.x, positionY, ingredient.radius, {
+    index: index,
+    render: {
+      sprite: { texture: ingredient.img, xScale: scale, yScale: scale },
     },
-  );
+    restitution: 0.3,
+    ...options,
+  });
 
   return body;
 };
@@ -114,13 +110,17 @@ const dropItem = (x) => {
   GAME.status = STATUS.DROP;
 
   // readyItem과 같은 모양의 droppedItem을 만들어 떨어뜨린 후 기존 readyItem 삭제
-  const droppedItem = newIngredient(x, false, currentIndex);
+  const droppedItem = newIngredient({ x }, currentIndex, {
+    isSleeping: false,
+    isStatic: false,
+  });
   Composite.add(engine.world, droppedItem);
-  GAME.score += BURGER[currentIndex].score;
+  GAME.score += BURGER[currentIndex].point;
   Composite.remove(engine.world, readyItem);
 
   // 새로운 readyItem 할당 후 0.5초 후 표시
-  readyItem = newIngredient(x);
+  readyItem = newIngredient({ x });
+  currentIndex = readyItem.index;
   setTimeout(() => {
     if (GAME.status === STATUS.DROP) {
       Composite.add(engine.world, readyItem);
@@ -131,7 +131,8 @@ const dropItem = (x) => {
 
 const startGame = () => {
   if (GAME.score > 0) return;
-  readyItem = newIngredient(GAME.width / 2);
+  readyItem = newIngredient({ x: GAME.width / 2 });
+  currentIndex = readyItem.index;
   Composite.add(engine.world, readyItem);
 };
 
@@ -156,6 +157,36 @@ Events.on(mouseConstraint, 'mousemove', (e) => {
 // 마우스 클릭 or 터치 후 drop
 Events.on(mouseConstraint, 'mouseup', (e) => {
   dropItem(e.mouse.position.x);
+});
+
+// 충돌시
+Events.on(engine, 'collisionStart', (e) => {
+  e.pairs.forEach((collision) => {
+    const { bodyA, bodyB } = collision;
+    // static, 마지막 아이템 충돌은 제외
+    if (bodyA.isStatic || bodyB.isStatic) return;
+    const lastIndex = BURGER.length - 1;
+    if (bodyA.index === lastIndex || bodyB.index === lastIndex) return;
+
+    if (bodyA.index === bodyB.index) {
+      // 충돌한 두개 삭제하고, 다음 인덱스 아이템을 충돌 위치에서 생성
+      const newIndex = bodyA.index + 1;
+      Composite.remove(engine.world, [bodyA, bodyB]);
+      const nextItem = newIngredient(
+        {
+          x: collision.collision.supports[0].x,
+          y: collision.collision.supports[0].y,
+        },
+        newIndex,
+        {
+          isSleeping: false,
+          isStatic: false,
+        },
+      );
+      Composite.add(engine.world, nextItem);
+      GAME.score += BURGER[newIndex].point;
+    }
+  });
 });
 
 const resize = () => {

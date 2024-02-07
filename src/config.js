bc2036c66cfb2c33836ebd5842abccaf8fda8cfb
hakyung <diveindeep.dev@gmail.com>
2024-02-07 22:01:31 +0900

    Add combo assets and styling

---
+++ src/config.js
@@ -0,0 +1,10 @@
+import combo from './js/combo.js';
+
+export const gameTitles = [
+  {
+    title: 'MAKE A COMBO',
+    id: 'combo',
+  },
+];
+
+export const gameInit = { combo };
---
+++ src/styles/variable.js
@@ -0,0 +1,12 @@
+export const COLORS = {
+  yellow: '#ffcd55',
+  red: '#de3c51',
+};
+
+export const SIZES = {
+  width: 600,
+  height: 900,
+  wallThickness: 15,
+  scoreHeight: 160,
+  footerHeight: 80,
+};
